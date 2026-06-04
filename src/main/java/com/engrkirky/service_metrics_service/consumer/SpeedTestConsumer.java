7cc9cd52224efcf3ec1b515ac38aa34b809c5787
Kirk Alyn Santos <kirkalyn13@gmail.com>
2026-06-04 02:07:10 -0400

    feat: speed test group id

--- src/main/java/com/engrkirky/service_metrics_service/consumer/SpeedTestConsumer.java
+++ src/main/java/com/engrkirky/service_metrics_service/consumer/SpeedTestConsumer.java
@@ -5,12 +5,13 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import static com.engrkirky.service_metrics_service.config.KafkaTopicConfig.SPEED_TEST_TOPIC;
+import static com.engrkirky.service_metrics_service.util.KafkaConstants.SERVICE_METRICS_PIPELINE_GROUP_ID;
 
 @Component
 public class SpeedTestConsumer {
     private static final Logger log = org.slf4j.LoggerFactory.getLogger(SpeedTestConsumer.class);
 
-    @KafkaListener(topics = SPEED_TEST_TOPIC, groupId = "service_metrics_pipeline")
+    @KafkaListener(topics = SPEED_TEST_TOPIC, groupId = SERVICE_METRICS_PIPELINE_GROUP_ID )
     public void listen(String message) {
         log.info("Received message: {}", message);
     }
--- src/main/java/com/engrkirky/service_metrics_service/util/KafkaConstants.java
+++ src/main/java/com/engrkirky/service_metrics_service/util/KafkaConstants.java
@@ -3,4 +3,6 @@ package com.engrkirky.service_metrics_service.util;
 public class KafkaConstants {
     public static final String PUBLISHED_STATUS = "published";
     public static final String FAILED_STATUS = "failed";
+
+    public static final String SERVICE_METRICS_PIPELINE_GROUP_ID = "service_metrics_pipeline";
 }
