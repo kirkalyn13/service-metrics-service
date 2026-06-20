313c52928e6edc105743866d6c4462099ed5bb6f
Kirk Alyn Santos <kirkalyn13@gmail.com>
2026-06-20 00:51:58 -0400

    refactor: run fmt and validate

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -6,21 +6,21 @@ module "networking" {
 }
 
 module "secrets" {
-  source      = "git::https://github.com/kirkalyn13/platform-infra.git//modules/secrets"
-  app_name    = var.app_name
-  api_key     = var.api_key
+  source   = "git::https://github.com/kirkalyn13/platform-infra.git//modules/secrets"
+  app_name = var.app_name
+  api_key  = var.api_key
 }
 
 module "ec2" {
-  source            = "git::https://github.com/kirkalyn13/platform-infra.git//modules/ec2"
-  app_name          = var.app_name
-  vpc_id            = module.networking.vpc_id
-  subnet_id         = module.networking.public_subnet_id
-  ami_id            = var.ami_id
-  instance_type     = var.instance_type
-  app_port          = var.app_port
-  app_jar_path      = var.app_jar_path
-  api_secret_arn    = module.secrets.secret_arn
+  source         = "git::https://github.com/kirkalyn13/platform-infra.git//modules/ec2"
+  app_name       = var.app_name
+  vpc_id         = module.networking.vpc_id
+  subnet_id      = module.networking.public_subnet_id
+  ami_id         = var.ami_id
+  instance_type  = var.instance_type
+  app_port       = var.app_port
+  app_jar_path   = var.app_jar_path
+  api_secret_arn = module.secrets.secret_arn
 }
 
 module "dns" {
